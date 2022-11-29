ae57a41e85915a1967d0bec03be4584d61540a8a
Raruto <Raruto@users.noreply.github.com>
2022-11-29 14:48:17 +0100

    increase custom color contrast for nav links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -46,6 +46,7 @@
     --secondary-hover: var(--custom-color, --secondary-hover);
     --contrast: var(--custom-color, --contrast-hover);
     --icon-chevron: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='rgb(255, 255, 255)' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='6 9 12 15 18 9'%3E%3C/polyline%3E%3C/svg%3E");
+    --primary-hover: #1ab3e6;
   }
 
   :root[data-color] body > nav:nth-of-type(1) {
