b3692db8de89c51359c95e526c7f3b10c7e7eb63
Raruto <Raruto@users.noreply.github.com>
2022-11-30 10:33:55 +0100

    show only `GroupsWithNoMacroGroup` as SuperGroup

--- frontend/src/store/group.ts
+++ frontend/src/store/group.ts
@@ -25,7 +25,7 @@ const getters: GetterTree<IGroupState, IRootState> = {
   search:                 (state): string                    => state.Search,
   activeGroup:            (state): SuperGroup | null         => state.ActiveGroup,
   /** @FIXME prevent Group->ID and Macrogroup->ID collisions (ref: Object.assign)  */
-  superGroups:            (state): ISuperGroupDict           => Object.assign({}, state.MacroGroups, state.Groups),
+  superGroups:            (state): ISuperGroupDict           => Object.assign({}, state.MacroGroups, state.GroupsWithNoMacroGroup),
   groupsWithNoMacroGroup: (state): IGroupDict                => state.GroupsWithNoMacroGroup,
   groupsInMacroGroup:     (state): (id: number) => Group[]   => (id: number) => state.GroupsInMacroGroups[id],
   groups:                 (state): IGroupDict                => state.Groups,
