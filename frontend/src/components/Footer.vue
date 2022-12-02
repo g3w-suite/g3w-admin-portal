f95fd6fdb1ef5d45da1be4e43d793aaa75ce5815
Raruto <Raruto@users.noreply.github.com>
2022-12-02 09:59:45 +0100

    update dialog

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -81,10 +81,10 @@
     </footer>
     <!-- TODO: add a reusable dialog component -->
     <dialog id="credits-modal">
-      <article>
+      <article style="max-width: unset;">
         <header style="background-color: rgb(149, 173, 54); margin-bottom: 1rem;">
           <form method="dialog">
-            <input type="submit" aria-label="Close" value="" class="close" />
+            <input type="submit" aria-label="Close" value="" class="close contrast" />
           </form>
           <!-- <a href="#close" aria-label="Close" class="close" ></a> -->
           <div style="color: #fff">Applicativo realizzato con il framework Open Source</div>
@@ -101,31 +101,35 @@
             </a>
           </p>
           <p>Pubblica e gestisci i tuoi progetti QGIS sul Web</p>
-          <p>
-            <a href="https://maps.google.com/?q=V.le%20G.%20Verdi%2051016,%20Montecatini%20Terme,%20Italy" target="_blank" rel="noopener noreferrer">
-              <font-awesome-icon icon="map-marker-alt" /> Montecatini Terme - Italy
-            </a>
-          </p>
-          <p>
-            <a href="tel:+393938534336">
-              <font-awesome-icon icon="phone-alt" /> +39 393 8534336
-            </a>
-          </p>
-          <p>
-            <a href="mailto:info@gis3w.it"> 
-              <font-awesome-icon icon="envelope" /> info@gis3w.it
-            </a>
-          </p>
+          <address style="white-space: nowrap;">
+            <span>
+              <a href="https://maps.google.com/?q=V.le%20G.%20Verdi%2051016,%20Montecatini%20Terme,%20Italy" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon icon="map-marker-alt" /> Montecatini Terme - Italy
+              </a>
+            </span>
+            &nbsp;
+            <span>
+              <a href="tel:+393938534336">
+                <font-awesome-icon icon="phone-alt" /> +39 393 8534336
+              </a>
+            </span>
+            &nbsp;
+            <span>
+              <a href="mailto:info@gis3w.it"> 
+                <font-awesome-icon icon="envelope" /> info@gis3w.it
+              </a>
+            </span>
+          </address>
+          <p></p>
         </section>
         <footer>
           <p>
             Framework sviluppato da
-            <a href="http://www.gis3w.it" target="_blank">
+            <a href="http://www.gis3w.it" target="_blank" title="Gis3w s.n.c">
               <img src="/static/client/images/logo_gis3w_156_85.png" alt="Gis3w s.n.c" width="60">
             </a>
           </p>
         </footer>
-        <!-- <p>test custom credits</p> -->
       </article>
     </dialog>
   </fragment>
