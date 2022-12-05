c28cfb79d759b0ba12b984929f0c2716018c00f3
Raruto <Raruto@users.noreply.github.com>
2022-12-05 16:37:41 +0100

    fix load more button styling (home page)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,19 +2,30 @@
   <fragment>
     <span class="subtitle h5 show-on-mobile">{{settings.sub_title}}</span>
     <h1 class="title show-on-mobile">{{settings.title}}</h1>
+    
+    <!-- TITLE AND DESSCRIPTION -->
     <hgroup>
       <h2>{{ settings.groups_title }}</h2>
       <p v-html="settings.groups_map_description"></p>
     </hgroup>
+    
+    <!-- FEATURED GROUPS -->
     <Projects
       :items="items"
       class="grid"
     />
-    <section id="home-show-more-button">
-      <a href="#" role="button" class="contrast outline">
-        <router-link to="/group">{{$t('messages.home.show_all_button')}}</router-link>
-      </a>
-    </section>
+    
+    <!-- LOAD MORE BUTTON -->
+    <div
+      v-if="items.length === maxItemstoShow"
+      style="text-align: center; margin: calc(var(--block-spacing-vertical)) auto;">
+      <router-link
+        role="button"
+        :to="{name: 'group'}"
+      >
+        {{$t('messages.home.show_all_button')}}
+      </router-link>
+    </div>
 
   </fragment>
 </template>
@@ -24,9 +35,7 @@ import Projects from '@/components/Projects.vue';
 import { Info } from '@/types/TInfo';
 import { Component, Vue } from 'vue-property-decorator';
 import { mapGetters } from 'vuex';
-import {SuperGroup} from "@/types/TSuperGroup";
-
-// const logo = require('@/assets/img/logo_g3wsuite.png');
+import { SuperGroup } from "@/types/TSuperGroup";
 
 @Component({
   components: { Projects },
@@ -38,26 +47,23 @@ import {SuperGroup} from "@/types/TSuperGroup";
 
 })
 export default class Home extends Vue {
+
   public settings!: Info;
+
+  public maxItemstoShow: number = 6;
+
   // get all super group (macro group and group not in macro group)
   public items: SuperGroup[] = this.$store.getters['group/superGroups'];
-  // used to show all items if more than 6
-  public showMoreButton: Boolean = false;
 
-  public created(){
+  public created() {
     // in case of length more than 6, show only 6 elements
-    if (this.items.length > 6) {
-      this.items = this.items.slice(0, 6);
-      this.showMoreButton = true;
+    if (this.items.length > this.maxItemstoShow) {
+      this.items = this.items.slice(0, this.maxItemstoShow);
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
-  #home-show-more-button {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px
-  }
 </style>
