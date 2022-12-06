b626e4f85cb4b571a104f1d48d0031b316fb07fc
volterra79 <boccacci.francesco@gmail.com>
2022-12-06 14:32:35 +0100

    set active group to null only if id params is not defined

--- frontend/src/views/MacroGroup.vue
+++ frontend/src/views/MacroGroup.vue
@@ -38,7 +38,7 @@ export default class VMacroGroup extends Vue {
     // Home > MacroGroups
     if (group || !id) {
       this.items = Object.values(this.$store.getters['group/macroGroups']);
-      this.$store.dispatch('group/setActiveGroup', { sg: null });
+      !id && this.$store.dispatch('group/setActiveGroup', { sg: null });
     }
     // Home > MacroGroup > ID
     // Home > MacroGroup > ID > SubGroup
