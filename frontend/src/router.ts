5820f4c7c2e0081471e7e9c95cd6d722a5000a1f
volterra79 <boccacci.francesco@gmail.com>
2022-12-06 08:59:56 +0100

    Change id macrogroup as optional

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -77,7 +77,7 @@ const router = new Router({
           },
         },
         {
-          path: 'organization/:id/:group?',
+          path: 'organization/:id?/:group?',
           name: 'organization',
           component: MacroGroup,
           meta: {
