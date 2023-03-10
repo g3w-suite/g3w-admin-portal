11e3ffa7127b612f14ce765d14560640a6548553
Raruto <Raruto@users.noreply.github.com>
2023-03-10 10:37:31 +0100

    Redirect to custom login and logout pages

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -223,6 +223,13 @@ export default class Navbar extends Vue {
   }
 
   public logout() {
+    // custom logout page
+    const { logout_url, login_url } = this.$store.getters['info/info'];
+    if ('login' !== login_url) {
+      location.href = logout_url;
+      return false;
+    }
+    // default logout
     this.$store.dispatch('me/logout', { locale: this.$i18n.locale });
   }
 
