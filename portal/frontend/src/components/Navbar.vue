4fcc770aba00dd036f6d8aee4ccca220dadeb398
Raruto <Raruto@users.noreply.github.com>
2023-04-13 10:01:12 +0200

    prevent default on logout link

--- portal/frontend/src/components/Navbar.vue
+++ portal/frontend/src/components/Navbar.vue
@@ -25,7 +25,7 @@
 
         <!-- LOGOUT LINK -->
         <li v-if="isLoggedIn">
-          <a href="#" @click="logout" :title="$t('messages.tooltip.logout')" class="secondary">
+          <a href="#" @click.prevent="logout" :title="$t('messages.tooltip.logout')" class="secondary">
             <font-awesome-icon icon="sign-out-alt" size="lg" />
             <span class="hide-on-mobile"> {{$t('messages.menu.logout')}}</span>
           </a>
@@ -107,7 +107,7 @@
 
         <!-- LOGOUT LINK -->
         <li v-if="!hasNavBarTop && isLoggedIn">
-          <a href="#" @click="logout" :title="$t('messages.tooltip.logout')" class="secondary">
+          <a href="#" @click.prevent="logout" :title="$t('messages.tooltip.logout')" class="secondary">
             <font-awesome-icon icon="sign-out-alt" size="lg" />
             <span class="hide-on-mobile"> {{$t('messages.menu.logout')}}</span>
           </a>
