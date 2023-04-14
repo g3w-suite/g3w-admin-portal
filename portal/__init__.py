632f49d4bad9a6edc4e661ef4bf57e7c4ea706d1
Raruto <Raruto@users.noreply.github.com>
2023-04-14 11:34:38 +0200

    Conditionally enable `portal.middleware.AuthByDRFTokenMiddleware`

--- portal/__init__.py
+++ portal/__init__.py
@@ -2,8 +2,22 @@ from django.conf import settings
 
 default_app_config = 'portal.apps.PortalConfig'
 
+# TODO: uninstall "django-samesite-none" in Django >= v3.1
 settings.MIDDLEWARE = [
     'django_samesite_none.middleware.SameSiteNoneMiddleware',
     'corsheaders.middleware.CorsMiddleware'
-] + settings.MIDDLEWARE 
-settings.THIRD_PARTY_APPS += ['corsheaders']
+] + settings.MIDDLEWARE
+
+# Conditionally enable `AuthByDRFTokenMiddleware`
+if 'rest_framework.authtoken' in settings.INSTALLED_APPS and \
+   'authjwt' in settings.INSTALLED_APPS :
+    settings.MIDDLEWARE += ['portal.middleware.AuthByDRFTokenMiddleware']
+
+# Activate `corsheaders` package through:
+# - THIRD_PARTY_APPS if we are running a G3W-ADMIN instance
+# - INSTALLED_APPS if we are running a plain DJANGO instance
+if hasattr(settings, 'THIRD_PARTY_APPS'):
+    settings.THIRD_PARTY_APPS += ['corsheaders'] 
+else:
+    settings.INSTALLED_APPS   += ['corsheaders']
+    
