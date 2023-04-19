8de6cebdca3f404e558eff871f7bbc15c0418e35
Raruto <Raruto@users.noreply.github.com>
2023-04-19 16:18:28 +0200

    group tests for `Home Page`

--- portal/frontend/tests/app.spec.ts
+++ portal/frontend/tests/app.spec.ts
@@ -1,18 +1,23 @@
 import { expect, test,  } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+test.describe('Home Page', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Home - G3W-SUITE/);
-});
+  test.beforeEach(async ({ page }) => {
+    // Navigate to Home Page
+    await page.goto('/');
+  });
 
-test('login link', async ({ page }) => {
-  await page.goto('/');
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Home - G3W-SUITE/);
+  });
 
-  // Click the login link.
-  await page.getByRole('link', { name: 'Login' }).click();
+  test('login link', async ({ page }) => {
+    // Click the login link.
+    await page.getByRole('link', { name: 'Login' }).click();
 
-  // Expects the URL to contain login.
-  await expect(page).toHaveURL(/.*login/);
+    // Expects the URL to contain login.
+    await expect(page).toHaveURL(/.*login/);
+  });
+
 });
