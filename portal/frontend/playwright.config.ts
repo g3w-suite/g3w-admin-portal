278ff46dd25621335786dbf9adbda8880b78f317
Raruto <Raruto@users.noreply.github.com>
2023-04-20 10:43:33 +0200

    rename folder: `tests` --> `e2e`

--- portal/frontend/playwright.config.ts
+++ portal/frontend/playwright.config.ts
@@ -8,10 +8,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 /**
+ * DEV server exposed by `npm run dev`
+ */
+const HTTP_SERVER = process.env.VITE_SERVER;
+
+/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
+  outputDir: './e2e/results',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -21,11 +27,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: './e2e/reports' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.VITE_SERVER,
+    baseURL: HTTP_SERVER,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,7 +80,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: process.env.VITE_SERVER,
+    url: HTTP_SERVER,
     reuseExistingServer: !process.env.CI,
   },
 });
