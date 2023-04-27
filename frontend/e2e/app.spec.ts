1ced00c2832864ec2a339b49c1673b5fd6bc7a88
Raruto <Raruto@users.noreply.github.com>
2023-04-27 09:35:25 +0200

    Update app.spec.ts

--- frontend/e2e/app.spec.ts
+++ frontend/e2e/app.spec.ts
@@ -20,4 +20,21 @@ test.describe('Home Page', () => {
     await expect(page).toHaveURL(/.*login/);
   });
 
+  test('authenticate user', async ({ page }) => {
+    // Click the login link.
+    await page.getByRole('link', { name: 'Login' }).click();
+
+    // Perform authentication steps.
+    await page.getByLabel('Username').fill('admin');
+    await page.getByLabel('Password').fill('admin');
+    await page.getByRole('button', { name: 'Accesso' }).click();
+
+    // Wait until the page reaches a state where all is set (eg. server cookies).
+    await page.waitForURL('**/it/')
+    await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+
+    // End of authentication steps.
+    await expect(page).toHaveTitle(/Home - G3W-SUITE/);
+  });
+
 });
