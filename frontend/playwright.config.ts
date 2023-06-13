77b8b1c09f01de682372c5843b1e2dc81edf750e
Raruto <Raruto@users.noreply.github.com>
2023-06-13 11:06:36 +0200

    Update playwright.config.ts

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -51,9 +51,9 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] },  },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] }, },
-    { name: 'webkit',   use: { ...devices['Desktop Safari'] },  },
+    { name: 'chromium', use: { ...devices['Desktop Chrome'], locale: 'en' },  },
+    { name: 'firefox',  use: { ...devices['Desktop Firefox'], locale: 'en' }, },
+    { name: 'webkit',   use: { ...devices['Desktop Safari'], locale: 'en' },  },
     // { name: 'Mobile Chrome',  use: { ...devices['Pixel 5'] },   },
     // { name: 'Mobile Safari',  use: { ...devices['iPhone 12'] }, },
     // { name: 'Microsoft Edge', use: { ...devices['Desktop Edge'], channel: 'msedge' },  },
