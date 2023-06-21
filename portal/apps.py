c234b7d61f2615833d45fe982d18d2decdaed816
Raruto <Raruto@users.noreply.github.com>
2023-06-21 09:13:08 +0200

    failsafe setup

--- portal/apps.py
+++ portal/apps.py
@@ -2,18 +2,31 @@
 from __future__ import unicode_literals
 from django.apps import AppConfig
 from django.conf import settings
+import logging
+
+logger = logging.getLogger('g3wadmin.debug')
 
 class PortalConfig(AppConfig):
     name = 'portal'
 
-    # TODO: uninstall "django-samesite-none" in Django >= v3.1
-    settings.MIDDLEWARE = [
-        'django_samesite_none.middleware.SameSiteNoneMiddleware',
-        'corsheaders.middleware.CorsMiddleware'
-    ] + settings.MIDDLEWARE
+    # OPTIONAL packages from "requirements_authjwt.txt"
+    try:
+        from corsheaders.middleware import CorsMiddleware
+        from django_samesite_none.middleware import SameSiteNoneMiddleware
+        logger.info('[PORTAL] setup optional packages: "corsheaders", "django_samesite_none"')
 
-    # Activate `corsheaders` package
-    if hasattr(settings, 'THIRD_PARTY_APPS'):
-        settings.THIRD_PARTY_APPS += ['corsheaders'] # when we are running a G3W-ADMIN instance 
-    else:
-        settings.INSTALLED_APPS   += ['corsheaders'] # when we are running a plain DJANGO instance
+        # TODO: uninstall "django-samesite-none" in Django >= v3.1
+        settings.MIDDLEWARE = [
+            'django_samesite_none.middleware.SameSiteNoneMiddleware',
+            'corsheaders.middleware.CorsMiddleware'
+        ] + settings.MIDDLEWARE
+
+        # Activate `corsheaders` package
+        if hasattr(settings, 'THIRD_PARTY_APPS'):
+            settings.THIRD_PARTY_APPS += ['corsheaders'] # when we are running a G3W-ADMIN instance 
+        else:
+            settings.INSTALLED_APPS   += ['corsheaders'] # when we are running a plain DJANGO instance
+
+    except ImportError:
+        logger.info('[PORTAL] optional requirements_authjwt.txt not installed, skipping setup...')
+        pass
