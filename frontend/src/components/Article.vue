830c0310d0813055a65616ef63c6d9c74469c731
Raruto <Raruto@users.noreply.github.com>
2024-04-12 15:39:23 +0200

    heading

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -147,7 +147,7 @@ export default class Article extends Vue {
         <form method="dialog"><input type="submit" aria-label="Close" value="" class="close contrast"></form>
         <h3 style="margin: 0;">${this.title}</h3>
       </header>
-      <h4>OGC <sup><a href="https://docs.qgis.org/3.34/${this.$i18n.locale}/docs/server_manual/services/ogcapif.html" target="_blank" style="font-size:0.75em">&#x2139;&#xFE0F;</a></sup></h4>
+      <h4>OGC API <sup><a href="https://docs.qgis.org/3.34/${this.$i18n.locale}/docs/server_manual/services/ogcapif.html" target="_blank" style="font-size:0.75em">&#x2139;&#xFE0F;</a></sup></h4>
       <ul>
         ${ Object.entries(this.ogc_urls).map(([name, url]) => `<li><b>${name}:</b> <a href="${get_admin_url(url)}" target="_blank">${get_admin_url(url)}</a></li>`).join('') }
       </ul>
