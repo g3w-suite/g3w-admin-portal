7d7cfe399c396fd6da817b07b1afdbb44331da09
Raruto <Raruto@users.noreply.github.com>
2026-04-23 14:42:29 +0200

    fix: missing fallback value for `PORTAL_LOGIN_URL` and `PORTAL_LOGIN_URL`

--- portal/api/serializers.py
+++ portal/api/serializers.py
@@ -212,8 +212,8 @@ class GenericSuiteDataSerializer(G3WRequestSerializer, GetUnlanguageFieldsMixin,
             ret['reset_password_url'] = reverse('password_reset')
 
         # add login_url and logout_url to view
-        portal_login_url = getattr(settings, 'PORTAL_LOGIN_URL')
-        login_url = getattr(settings, 'LOGIN_URL')
+        portal_login_url = getattr(settings, 'PORTAL_LOGIN_URL', None)
+        login_url = getattr(settings, 'LOGIN_URL', None)
 
         if portal_login_url:
             ret['login_url'] = f"{settings.PORTAL_LOGIN_URL}"
